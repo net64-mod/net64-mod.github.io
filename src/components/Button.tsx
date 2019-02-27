202c03e41077b6d0b3a4066e4faa9fa18d0a5615
Mario Reder <mreder1289@gmail.com>
2019-02-27 08:07:58 +0100

    ci

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
 
-import { widths } from '../styles/variables'
 import { getEmSize } from '../styles/mixins'
 import { Link } from 'gatsby'
 
